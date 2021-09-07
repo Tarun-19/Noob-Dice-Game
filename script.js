b53625a52e0d20038a2c302861bfe7798c7033b3
Tarun-19 <63391690+Tarun-19@users.noreply.github.com>
2021-09-07 09:40:32 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,12 +1,12 @@
 var randomNumber1 = Math.random() * 6 + 1;
 randomNumber1 = Math.floor(randomNumber1);
-var randomImageSource1 = "/assets/" + "dice" + randomNumber1 + ".png";
+var randomImageSource1 = "/Noob-Dice-Game/assets/" + "dice" + randomNumber1 + ".png";
 var image1 = document.querySelectorAll("img")[0];
 image1.setAttribute("src", randomImageSource1);
 
 var randomNumber2 = Math.random() * 6 + 1;
 randomNumber2 = Math.floor(randomNumber2);
-var randomImageSource2 = "/assets/" + "dice" + randomNumber2 + ".png";
+var randomImageSource2 = "/Noob-Dice-Game/assets/" + "dice" + randomNumber2 + ".png";
 var image2 = document.querySelectorAll("img")[1];
 image2.setAttribute("src", randomImageSource2);
 
